d62a966f66c342801931d88bedfa3ef8e9a5ff98
turbodivx <dawid.sielski@outlook.com>
2018-08-30 23:21:11 +0200

    Initial commit for develop branch

--- index.py
+++ index.py
@@ -1,21 +1,12 @@
 import os
-import dash
-
-import dash_core_components as dcc
-import dash_html_components as html
-import dash_table_experiments as dt
 
 from flask import Flask, render_template
-# from runs import run_information
-
 
 
 DATA_FOLDER = 'test-data/results-archive'
 DATA_SUMMARY_FOLDER = 'qc'
 
 server = Flask(__name__)
-# runs_dash = dash.Dash(__name__, server=server, url_base_pathname='/dash')
-# runs_dash.layout = run_information('171030_NB551023_0034_AHYF5YBGX2')
 
 
 @server.route("/")
@@ -32,8 +23,6 @@ def runs():
 @server.route("/runs/<run_id>")
 def specific_run(run_id):
     return render_template('run.html', run_id=run_id)
-    # run_info = run_information(run_id)
-    # return run_info.index()
 
 
 if __name__ == '__main__':
