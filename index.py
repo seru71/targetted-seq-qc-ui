920b0fe5fbeb0fda484d25c6c4602698cbb85347
turbodivx <dawid.sielski@outlook.com>
2018-08-31 14:25:32 +0200

    Samples update

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
 import os
+import pandas as pd
 
-from flask import Flask, render_template
 import data_preparation
 
+from flask import Flask, render_template, request
 
-DATA_FOLDER = 'test-data/results-archive'
+DATA_FOLDER = os.path.join(*['test-data', 'results-archive'])
 DATA_SUMMARY_FOLDER = 'qc'
 
 server = Flask(__name__)
@@ -21,10 +22,82 @@ def runs():
     return render_template('runs.html', runs=runs)
 
 
-@server.route("/runs/<run_id>")
+@server.route("/samples")
+def samples():
+    runs = os.listdir(DATA_FOLDER)
+    samples = {}
+
+    def valid_sample(sample_name):
+        try:
+            return True if int(sample_name) else False
+        except Exception:
+            return False
+
+    for run_id in runs:
+        run_path = os.path.join(DATA_FOLDER, run_id)
+        samples[run_id] = ([(run_id, sample_id) for sample_id in os.listdir(run_path) if valid_sample(sample_id)])
+
+    return render_template('samples.html', samples=samples)
+
+
+@server.route("/samples/<run_id>/<sample_id>", methods=['GET', 'POST'])
+def specific_sample(run_id, sample_id):
+    data = {'run_id': run_id, 'sample_id': sample_id}
+
+    sample_path = os.path.join(DATA_FOLDER, run_id, sample_id)
+    coverage_sample_summary_path = (os.path.join(sample_path, f'{sample_id}.coverage.sample_summary'))
+    coverage_sample_gene_summary_path = (os.path.join(sample_path, f'{sample_id}.coverage.sample_gene_summary'))
+
+    coverage_sample_summary = pd.read_csv(coverage_sample_summary_path, delimiter='\t', index_col=False).dropna()
+    coverage_sample_gene_summary = pd.read_csv(coverage_sample_gene_summary_path, delimiter='\t', index_col=False)[:100]
+
+    data['coverage_sample_summary'] = coverage_sample_summary.to_html()
+
+    if request.method == 'POST' and request.form.get('gene_names'):
+        try:
+            data['genes'] = [x.strip() for x in request.form.get('gene_names').split(',')]
+
+            coverage_sample_gene_summary['gene_name'] = coverage_sample_gene_summary.Gene.apply(lambda x: x.split('_')[0])
+            df = coverage_sample_gene_summary.loc[coverage_sample_gene_summary['gene_name'].isin(data['genes'])]
+
+            df.drop(columns=['gene_name'], inplace=True)
+
+            df = data_preparation.prepare_mean_columns_df(df)
+
+            data['coverage_sample_gene_summary'] = df.to_html()
+        except Exception as e:
+            print(e)
+            pass
+
+    return render_template('sample.html', **data)
+
+
+@server.route("/runs/<run_id>", methods=['GET', 'POST'])
 def specific_run(run_id):
-    sample_summary_table = data_preparation.get_summary(run_id).to_html()
-    return render_template('run.html', run_id=run_id, sample_summary_table=sample_summary_table)
+    sample_summary_table = data_preparation.get_summary(run_id)
+    mean_cols_df = data_preparation.get_gene_summary(run_id)
+
+    unique_genes = set([x.split('_')[0] for x in mean_cols_df.Gene])
+
+    data = {'run_id': run_id, 'sample_summary_table': sample_summary_table.to_html(), 'unique_genes': unique_genes}
+
+    if request.method == 'POST' and request.form.get('gene_names'):
+        try:
+            data['genes'] = [x.strip() for x in request.form.get('gene_names').split(',')]
+
+            mean_cols_df['gene_name'] = mean_cols_df.Gene.apply(lambda x: x.split('_')[0])
+            df = mean_cols_df.loc[mean_cols_df['gene_name'].isin(data['genes'])]
+
+            df.drop(columns=['gene_name'], inplace=True)
+
+            df = data_preparation.prepare_mean_columns_df(df)
+
+            data['selected_genes_df'] = df.to_html()
+        except Exception as e:
+            print(e)
+            pass
+
+    return render_template('run.html', **data)
 
 
 if __name__ == '__main__':
