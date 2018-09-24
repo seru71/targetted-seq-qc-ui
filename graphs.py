3adda79ececcdd7b3fe1d0f0c40ebacd37d7cbc5
turbodivx <dawid.sielski@outlook.com>
2018-09-24 22:18:46 +0200

    fixed no graph while no stats file presented

--- graphs.py
+++ graphs.py
@@ -23,32 +23,34 @@ def sample_summary_graph(df):
 
 
 def variants_graph(df):
-    x_label_variants = ['Sample ' + x for x in df['Sample'].astype('str')]
-    return dict(
-        data=[
-            dict(
-                go.Bar(
-                    x=x_label_variants,
-                    y=df.nNonRefHom,
-                    name='nNonRefHom',
+    if df:
+        x_label_variants = ['Sample ' + x for x in df['Sample'].astype('str')]
+        return dict(
+            data=[
+                dict(
+                    go.Bar(
+                        x=x_label_variants,
+                        y=df.nNonRefHom,
+                        name='nNonRefHom',
+                    )
+                ),
+                dict(
+                    go.Bar(
+                        x=x_label_variants,
+                        y=df.nHets,
+                        name='nHets',
+                    )
+                ),
+                dict(
+                    go.Bar(
+                        x=x_label_variants,
+                        y=df.nIndels,
+                        name='nIndels',
+                    )
                 )
-            ),
-            dict(
-                go.Bar(
-                    x=x_label_variants,
-                    y=df.nHets,
-                    name='nHets',
-                )
-            ),
-            dict(
-                go.Bar(
-                    x=x_label_variants,
-                    y=df.nIndels,
-                    name='nIndels',
-                )
+            ],
+            layout=go.Layout(
+                barmode='stack'
             )
-        ],
-        layout=go.Layout(
-            barmode='stack'
         )
-    )
+    return None
