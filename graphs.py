1991815b4900d24fe7de35805cc55b4759a6e607
turbodivx <dawid.sielski@outlook.com>
2018-09-24 22:15:31 +0200

    both plots added. run page without variants file is not rendering in this moment.

---
+++ graphs.py
@@ -0,0 +1,54 @@
+import plotly.graph_objs as go
+
+
+def sample_summary_graph(df):
+    x_label = ['Sample ' + x for x in df['Sample ID'].astype('str')]
+    return dict(
+        data=[
+            dict(
+                go.Bar(
+                    x=x_label,
+                    y=df['Mean'],
+                    name='Mean'
+                )
+            ),
+            dict(
+                go.Bar(
+                    x=x_label,
+                    y=df['Above 20%'],
+                    name='Above 20%')
+            )
+        ]
+    )
+
+
+def variants_graph(df):
+    x_label_variants = ['Sample ' + x for x in df['Sample'].astype('str')]
+    return dict(
+        data=[
+            dict(
+                go.Bar(
+                    x=x_label_variants,
+                    y=df.nNonRefHom,
+                    name='nNonRefHom',
+                )
+            ),
+            dict(
+                go.Bar(
+                    x=x_label_variants,
+                    y=df.nHets,
+                    name='nHets',
+                )
+            ),
+            dict(
+                go.Bar(
+                    x=x_label_variants,
+                    y=df.nIndels,
+                    name='nIndels',
+                )
+            )
+        ],
+        layout=go.Layout(
+            barmode='stack'
+        )
+    )
