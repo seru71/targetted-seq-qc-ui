08daf4a83f84e0f2396927630d410d1bac9ac794
turbodivx <dawid.sielski@outlook.com>
2018-10-01 17:40:08 +0200

    round update

--- ngs/paths_processing.py
+++ ngs/paths_processing.py
@@ -42,13 +42,6 @@ def get_sample_gene_summary_path(run_id, sample_id):
     return os.path.join(sample_path, '{}.coverage.sample_gene_summary'.format(sample_id))
 
 
-def get_fastq_paths(run_id, sample_id):
-    run_path = os.path.join(FOLDER_WITH_RUNS, run_id)
-    fastq_path_r1 = os.path.join(run_path, FASTQ, '{}_S1_R1_001.fastq.gz'.format(sample_id))
-    fastq_path_r2 = os.path.join(run_path, FASTQ, '{}_S1_R2_001.fastq.gz'.format(sample_id))
-    return fastq_path_r1, fastq_path_r2
-
-
 def get_fastq_r1_download_path(run_id, sample_id):
     return os.path.join(run_id, FASTQ, '{}_S1_R1_001.fastq.gz'.format(sample_id))
 
@@ -79,19 +72,6 @@ def get_run_path(run_id):
 
 def get_sample_path(run_id, sample_id):
     return os.path.join(get_run_path(run_id), sample_id)
-
-
-def get_bam_path(run_id, sample_id):
-    return os.path.join(get_sample_path(run_id, sample_id), '{}.dedup.bam'.format(sample_id))
-
-
-def get_vcf_path(run_id, sample_id):
-    return os.path.join(get_sample_path(run_id, sample_id), '{}.vcf'.format(sample_id))
-
-
-def get_fq_fastqc_path(run_id, sample_id, fq):
-    reports_path = os.path.join(FOLDER_WITH_RUNS, run_id, DATA_SUMMARY_FOLDER, READ_QC)
-    return os.path.join(reports_path, '{}.fq{}_fastqc.html'.format(sample_id, fq))
 
 
 def get_annotated_variants_stats_path(run_id):
